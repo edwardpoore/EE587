c53370e0d91c20ba4c539ae173929df184c82192
Edward Poore <edward.poore@gmail.com>
2012-03-08 20:56:15 -0500

    got interrupts working. need oscope

--- lab5/part1/init3.c
+++ lab5/part1/init3.c
@@ -25,14 +25,7 @@ sfr16    TMR3     = 0x94;              // Timer3 counter registers
 #define  SMB_FREQUENCY  200000L         // Target SCL clock rate
                                        // This example supports between 10kHz
                                        // and 100kHz
-#define  EEPROM_ADDR    0xA0           // Device address for slave target
-                                       // Note: This address is specified
-                                       // in the Microchip 24LC02B
-                                       // datasheet.
 
-#define  MY_ADDR        0x02           // Address of this SMBus device
-                                       // (dummy value since this device does
-                                       // not have any defined slave states)
 
 
 
@@ -59,24 +52,7 @@ void SYSCLK_Init (void)
                                        // source and enable missing clock
                                        // detector
 }
-/*
-//-----------------------------------------------------------------------------
-// PORT_Init
-//-----------------------------------------------------------------------------
-//
-// Configure the Crossbar and GPIO ports
-//
-void PORT_Init (void)
-{
-   XBR0    = 0x05;                     // Enable UART0 and SMBUS
-   XBR1    = 0x80;
-   P1MDOUT = 0x03;                    // enable P1.6 (LED), P1.5 (SCLK) as push-pull output
-                                      // P1.7 (SDA) open drain
-   
-   XBR2    = 0x44;                     // Enable crossbar and weak pull-ups
 
-}
-*/
 //-----------------------------------------------------------------------------
 // UART0_Init
 //-----------------------------------------------------------------------------
@@ -93,51 +69,3 @@ void UART0_Init (void)
    PCON  |= 0x80;                      // SMOD00 = 1
    TI0    = 1;                         // Indicate TX0 ready
 }
-/*
-//-----------------------------------------------------------------------------
-// SMBus_Init
-//-----------------------------------------------------------------------------
-//
-// Return Value : None
-// Parameters   : None
-//
-// The SMBus peripheral is configured as follows:
-// - SMBus enabled
-// - Assert Acknowledge low (AA bit = 1b)
-// - Free and SCL low timeout detection enabled
-//
-void SMBus_Init (void)
-{
-     SMB0CN = 0x41;
-     SMB0CR = 0x99;
-}
-//-----------------------------------------------------------------------------
-// Timer3_Init
-//-----------------------------------------------------------------------------
-//
-// Return Value : None
-// Parameters   : None
-//
-// Timer3 configured for use by the SMBus low timeout detect feature as
-// follows:
-// - SYSCLK/12 as Timer3 clock source
-// - Timer3 reload registers loaded for a 25ms overflow period
-// - Timer3 pre-loaded to overflow after 25ms
-// - Timer3 enabled
-//
-void Timer3_Init (void)
-{
-   TMR3CN = 0x02;                      // Timer3 uses SYSCLK/12
-
-   TMR3RL =  0xAF;           // Timer3 configured to overflow after
-   TMR3RLH = 0xFD;
-   EIE2 |=   0x01;                       // Timer3 interrupt enable
-   TMR3CN |= 0x02;                     // Start Timer3
-}
-void Interrupts_Init(void)
-{
-   EIE1      = 0x02;    
-   EIE2      = 0x01;
-
-
-}*/
