3d1268fc8e3a65f34686095a312ab7b7cdfc8e6f
Edward Poore <edward.poore@gmail.com>
2012-03-23 13:42:59 -0400

    works

--- lab6/partTwo/main.c
+++ lab6/partTwo/main.c
@@ -1,5 +1,5 @@
 //Edward Poore and Danny Peters
-//Pin 1.0 is the pin toggled in main
+
   
 #include <compiler_defs.h>
 #include <C8051F020_defs.h>                 // SFR declarations
@@ -8,10 +8,13 @@
 
 
 
-unsigned char in_char = 'i'; //inbyte
-unsigned char out_char = 'o'; //outbyte
+unsigned char transmit = 0; //transmit flag
 unsigned char status = 0x00; //holds status register info
 sbit rReady = P1^0; //receiver ready line
+unsigned int bufferPointer = 0x00; //pointer to places in the buffer
+unsigned char buffer[64]; //character buffer
+unsigned char buffCount = 0; 
+unsigned int counter = 0x00;
 
 void main(void){
  
@@ -19,7 +22,7 @@ void main(void){
   WDTCN = 0xad;
   SYSCLK_Init ();                     // initialize oscillator
   UART1_Init ();                      // initialize UART1
-  XBR0 = 0x00;                     // Route UART to GPIO
+  XBR0 = 0x04;                     // Route UART to GPIO
   XBR1 = 0x00;
   XBR2 = 0xC4;                     // Enable crossbar
   P1MDOUT = 0x00;                  //1.0 is open drain
@@ -28,22 +31,19 @@ void main(void){
   rReady = 0x01; //set the bit high so that it acts as an input
   
 
-  unsigned char chaReady = 0x00; //flag to store a character
-  unsigned int bufferPointer = 0x00; //pointer to places in the buffer
-  unsigned char buffer[64]; //character buffer
+
   while(1)
   {
-    if(chaReady = 0x01)
+    if(bufferPointer != 0x00 && rReady == 0x00 && buffCount > 0x00) //if there is data in the buffer and the ready line is low
     {
-      
+      SBUF1 = buffer[0]; //send the character
+      transmit = 0x01;
+      while(transmit == 0x01){} //wait for the data to be sent
+    }
     
-  
-  
-  
-  
   }
     
-}; //main    
+} //main    
    
 
 void isr_uart1(void) interrupt 20 {
@@ -51,17 +51,28 @@ void isr_uart1(void) interrupt 20 {
   status = SCON1 & 0x03; // SCON1 & 0b00000011 to get TI1, RI1
   if(status == 0x01) //RI1 set
   {
-    in_char = SBUF1; //get data from UART1 receive latch
+    if(bufferPointer == 0)
+      bufferPointer++;
+    buffer[bufferPointer-1] = SBUF1; //get data from UART1 receive latch
+    if(!(bufferPointer > 64))
+      bufferPointer++;
+      buffCount++;
     SCON1 = 0x50; //clear RI1
   }
   
   if(status == 0x02) //TX1 set
   {
-    SBUF1 = out_char; //place data in the transmit buffer
+    bufferPointer--; //decrement the pointer for the sent byte
+    buffCount--;
+    for(counter = 0x00; counter < 63; counter++)
+    {   
+        buffer[counter] = buffer[counter+1];
+    }
+    transmit = 0x00; //clear xmit flag
     SCON1 = 0x50; //clear TI1
   }
 
-}; //isr_uart1
+} //isr_uart1
 
 
 
